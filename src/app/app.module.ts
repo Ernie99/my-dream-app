d7199174aa1e9f9795bfbd46681184231b66848f
Ernie <ernest.homsy@parsons.com>
2016-10-21 18:21:08 -0400

    halfway thru routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { AppComponent } from './app.component';
 import {HeroService} from "./hero.service";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -15,15 +16,21 @@ import {HeroService} from "./hero.service";
     HeroesComponent,
     HeroDetailComponent,
     AppComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-    {
-      path: 'heroes',
-      component: HeroesComponent
-    }
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+
     ])],
   providers: [HeroService],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-dashboard',
+  // templateUrl: './dashboard.component.html',
+  template: '<h3>My Dahsboard</h3>',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+  constructor() { }
+
+
+}
